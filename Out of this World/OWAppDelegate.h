9c6ec82df864b520b7dbc17192ee16b2011da1ad
Jose Manuel Ramírez Martínez <jmramirez@motofan.com>
2014-09-14 13:05:44 +0200

    Initial Commit

---
+++ Out of this World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this World
+//
+//  Created by Jose Manuel Ramirez Martinez on 14/09/14.
+//  Copyright (c) 2014 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/OWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWViewController.h
+//  Out of this World
+//
+//  Created by Jose Manuel Ramirez Martinez on 14/09/14.
+//  Copyright (c) 2014 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWViewController : UIViewController
+
+@end
