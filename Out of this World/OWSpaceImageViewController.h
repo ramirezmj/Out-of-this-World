514ca43e3153eb1a84f400f05407b0b782675583
Jose Manuel Ramírez Martínez <jmramirez@motofan.com>
2014-09-15 21:19:42 +0200

    Added a Scroll View

---
+++ Out of this World/OWSpaceImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWSpaceImageViewController.h
+//  Out of this World
+//
+//  Created by Jose Manuel Ramirez Martinez on 15/09/14.
+//  Copyright (c) 2014 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWSpaceImageViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
---
+++ Out of this World/OWSpaceImageViewController.m
@@ -0,0 +1,13 @@
+//
+//  OWSpaceImageViewController.m
+//  Out of this World
+//
+//  Created by Jose Manuel Ramirez Martinez on 15/09/14.
+//  Copyright (c) 2014 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import "OWSpaceImageViewController.h"
+
+@implementation OWSpaceImageViewController
+
+@end
